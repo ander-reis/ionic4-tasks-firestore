0ca136601a64d01086dd09589d59b0125b837327
Desktop Casa <ander-reis@hotmail.com>
2019-06-16 18:22:14 -0300

    Mensagens de erro de validação com ion-note

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+    authForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.createForm();
+    }
+
+    private createForm(): void {
+        this.authForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    get email(): FormControl {
+        return <FormControl> this.authForm.get('email');
+    }
+
+    get password(): FormControl {
+        return <FormControl> this.authForm.get('password');
+    }
+
+    onSubmit(): void {
+        console.log('AuthForm', this.authForm.value);
+    }
+}
